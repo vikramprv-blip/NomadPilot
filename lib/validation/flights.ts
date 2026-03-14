add784029bf42a3708de099c56bc6563f5af0a58
vikramprv-blip <vikramprv@gmail.com>
2026-03-14 22:57:06 +0530

    Update flights.ts

--- lib/validation/flights.ts
+++ lib/validation/flights.ts
@@ -1,4 +1,4 @@
-// lib/validation/flights.ts
 export * from "./flights.input";
 export * from "./flights.output";
 export * from "./helpers";
+export { FlightSearchInputSchema as searchSchema } from "./flights.input";
